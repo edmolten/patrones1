85690b1e6ac13f4ae08c8fc429abb376826b55a9
Eduardo Ramírez Farias <eedmolteen@gmail.com>
2016-10-01 19:45:00 -0300

    test

--- tarea1.py
+++ tarea1.py
@@ -1,1 +1,18 @@
-datasetFile = open()
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure()
+ax = Axes3D(fig)
+
+datasetFile = open("swissroll.dat", "r")
+xs = []
+ys = []
+zs = []
+for line in datasetFile:
+    x, y, z = line.split()
+    xs.append(float(x))
+    ys.append(float(y))
+    zs.append(float(z))
+
+ax.scatter(xs, ys, zs)
+plt.show()
